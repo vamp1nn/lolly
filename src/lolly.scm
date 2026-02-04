6f290e7aa8670063a4f8288f1b78b58cc68a4838
vamp1nn <vamp@vm.fail>
2026-02-04 10:30:00 +0000

    split the implementation into a proper scheme module

--- src/lolly.scm
+++ src/lolly.scm
@@ -1,2 +1,8 @@
-;;; this file is the source-tree entry point for the lolly library.
-(load "lolly-core.scm")
+;;; this file is the public guile module entry point for the lolly library.
+(define-module (lolly)
+	#:export (lolly-read-grammar lolly-grammar-file lolly-build lolly-parser
+						lolly-parse lolly-generate lolly-main)
+	#:declarative? #f)
+(if (file-exists? "src/lolly-core.scm")
+	(primitive-load "src/lolly-core.scm")
+	(primitive-load "lolly-core.scm"))
